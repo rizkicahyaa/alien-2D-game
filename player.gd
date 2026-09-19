b03d18a688eb43cbeb63c8f7097500e3f00ce5b8
Rizki Cahya <rizki.cahya03@gmail.com>
2026-09-19 17:52:01 +0700

    feat: flip player (up/down)

--- player.gd
+++ player.gd
@@ -29,3 +29,6 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
